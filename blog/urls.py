c023b8d18f943bfae160fe502192adb809956cf9
iminlan <38048764+iminlan@users.noreply.github.com>
2018-07-11 22:45:14 +0500

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+from blog.articles.views import home
+from blog.articles.views import calculate
+from blog.articles.views import archiv, articles_num, articles_year
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
-    path('/', include('articles.urls')),    
+    path('calculate/', calculate),
+    path('articles/', archiv),  # для вывода архива
+    path('articles/<int:id>/', articles_num, name='articles_num'), # для вывода статьи с номером
+    path('articles/archive/<int:year>/', articles_year, name='articles_year'),  # для вывода статей по году
 
 ]
